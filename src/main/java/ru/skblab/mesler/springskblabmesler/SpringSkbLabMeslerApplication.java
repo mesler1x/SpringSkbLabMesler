b77e5eb6b688217f55695f38ee217f01f958dddc
Roman Mesler <pay2mesler@gmail.com>
2024-02-26 21:42:15 +0500

    init

---
+++ src/main/java/ru/skblab/mesler/springskblabmesler/SpringSkbLabMeslerApplication.java
@@ -0,0 +1,13 @@
+package ru.skblab.mesler.springskblabmesler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSkbLabMeslerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSkbLabMeslerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skblab/mesler/springskblabmesler/SpringSkbLabMeslerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skblab.mesler.springskblabmesler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSkbLabMeslerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
